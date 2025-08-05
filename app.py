68d918e43fbef9106dd89ca4f447dd142831c2d7
Pratham Shetty <prathamshetty303@gmail.com>
2025-08-05 16:47:28 +0000

    webhook addition

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "🚀 Hello from Flask DevOps Pipeline! this is my start of devops tour"
+    return "🚀 Hello from Flask DevOps Pipeline! this is my start of devops tourand this is weebhook automation"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
