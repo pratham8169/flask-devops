42a6a03c35ef81470120a274db15918cbc6b191b
Pratham Shetty <prathamshetty303@gmail.com>
2025-08-06 10:28:50 +0000

    new changes to app.py

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "🚀 Hello from Flask DevOps Pipeline! this is my start of devops tourand this is weebhook automation"
 
+@app.route('/about')
+def about():
+    return "This is about page of my flask app. Pratham Shetty"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
